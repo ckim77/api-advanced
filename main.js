f7e0955371019c6ad9c8594c552643e7e448ab78
Chris Kim <ckjr92@gmail.com>
2021-11-03 14:12:23 -0600

    finished all questions

--- main.js
+++ main.js
@@ -3,7 +3,15 @@ const getResidentsBtn = document.getElementById('get-residents');
 const getResidentsFunction = () => {
     axios.get("https://swapi.dev/api/planets/2")
     .then((res) => {
-        console.log(res.data);
+        for (i = 0; i < res.data.residents.length; i++) {
+            axios.get(res.data.residents[i])
+            .then ((response) => {
+                console.log(response.data);
+                let header = document.createElement('h2');
+                header.textContent = response.data.name;
+                document.body.appendChild(header);
+            })
+        }
     })
 }
 
